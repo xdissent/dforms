305d1c4bb44234b32b8d5f3832b5b792d2557765
Greg Thornton <xdissent@gmail.com>
2009-12-21 08:22:37 -0600

    Fixed attributes util for non-array attrs.

--- src/DForms/Utils/Attributes.php
+++ src/DForms/Utils/Attributes.php
@@ -4,6 +4,11 @@ class DForms_Utils_Attributes
 {
     public static function flatten($attrs) {
         $flat = '';
+        
+        if (!is_array($attrs)) {
+            return $flat;
+        }
+        
         foreach ($attrs as $name => $value) {
             $flat .= sprintf(' %s="%s"', $name, htmlentities($value));
         }
